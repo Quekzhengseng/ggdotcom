d3ba648f21d4c909cbfb3d9a9fe83ef6c6fff065
Dwight Chong Jia Hao <dwightchong.2023@scis.smu.edu.sg>
2025-01-12 16:02:03 +0800

    new routing to new pages

--- frontend/app/src/router/index.js
+++ frontend/app/src/router/index.js
@@ -2,11 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Chat from '../views/Chat.vue'
 import History from '../views/History.vue'
+import SimulatorChat from '../views/SimulatorChat.vue';
+import Selection from '../views/Selection.vue'
+
 
 const routes = [
   { path: '/', component: Home },
   { path: '/chat', component: Chat },
   { path: '/history', component: History },
+  { path: '/simulator', name: 'Simulator', component: SimulatorChat },
+  { path: '/selection', name:'Selection', component: Selection}
 ]
 
 const router = createRouter({
