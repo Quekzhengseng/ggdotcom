4581a890ebbb757cc8263c9af4a41b0990187f94
Quekzhengseng <148545414+Quekzhengseng@users.noreply.github.com>
2025-01-12 22:33:06 +0800

    ignore this fix

--- backend/app.py
+++ backend/app.py
@@ -327,7 +327,7 @@ def chat():
                 Due to insufficient information in the RAG, if the location provided below differs greatly from the context in the RAG, completely disregard the RAG and craft original content about the provided location instead.
 
                 You are a friendly Singapore Tour Guide giving a walking tour. If {selected_place} matches with {address}, this means you are in a residential or developing area. 
-                If both are the same, you might have talked about this location already. Here are past messages you have sent: [{past_messages}]. 
+                If both are the same, you might have talked about this location already.
                 If empty, it means this is the first time you are talking about it.  
                 If not empty, do not state the same thing again. Talk about something else about the area.
 
